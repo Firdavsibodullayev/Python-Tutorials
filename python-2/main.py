856cd89b08bfff9e98a70ae4eddd84d0a896cb49
FirdavsIbodullayev <firdavsibodullayev773@gmail.com>
2023-08-28 10:37:31 +0500

    change

---
+++ python-2/main.py
@@ -0,0 +1,63 @@
+# son1 = input('Sonni Kiriting:')
+# son2 = input('Sonni Kiriting:')
+
+# natija = int(son1) ** int(son2)
+
+# print(f'{son1} ning {son2}-darajasi {natija} ga teng') 
+# yosh = int(input('Yoshingizni kiriting: \n>>>'))
+
+# if yosh >= 18:
+#     print('Siz ovoz bera olasiz')
+# else:
+#     print('Siz ovoz bera olmaysiz')
+
+# son1 = input('Birinchi sonni kiriting:')
+# son2 = input('Ikkinchi sonni kiriting:')
+
+# if son1 > son2:
+#     print(f'{son1} katta {son2} dan')
+# elif son1 == son2:
+#     print(f'{son1} teng {son2} ga')
+# else:
+
+#     print(f'{son2} katta {son1} dan')
+
+# til = input('Tilni tanlang: en/uz? ')
+
+# if til == 'uz':
+#     print('Assalomu aleykum')
+# elif til == 'en':
+#     print('Hello Sheet')
+# else:
+#     print('uzbek va ingliz tilini tanlang')
+
+
+# sum = 0
+# for num in range(101):
+#     sum += num 
+
+# print(sum)
+
+
+# for i in range(10):
+#     yulduzcha += str(i)
+#     print(yulduzcha)
+
+
+ 
+# ismlar = ['Shoxrux', 'Hamidjon', 'Firdavs', 'Shoxjaxon', 'Shoxruh', 'Islombek', 'Mehroj', 'Javohir']
+# for i in ismlar:
+#      print(f"Salom {i}")
+
+
+# yulduzcha = '*'
+
+# for i in range(10):
+#     yulduzcha += str(i)
+#     print(yulduzcha)
+
+# for index in range(10):
+#     if index % 3:
+#         continue
+
+#     print(index)
